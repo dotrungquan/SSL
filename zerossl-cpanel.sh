344832e56818d2809d11d57eba98c12314e8733c
dotrungquan <56627093+dotrungquan@users.noreply.github.com>
2022-12-10 17:32:22 +0700

    Update zerossl-cpanel.sh

--- zerossl-cpanel.sh
+++ zerossl-cpanel.sh
@@ -6,5 +6,5 @@ read -p "Nhap vao Document Root: " document_root
 wget -O - https://get.acme.sh | sh -s email=info@'domain'
 source ~/.bashrc
 
-/home/$user/.acme.sh/acme.sh --issue --webroot ~/"document_root" -d "$domain" -d www."$domain" --force
+/home/$user/.acme.sh/acme.sh --issue --webroot ~/"$document_root" -d "$domain" -d www."$domain" --force
 /home/$user/.acme.sh/acme.sh --deploy --deploy-hook cpanel_uapi --domain "$domain"  --domain www."$domain"
