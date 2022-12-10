ee7867ae3ae0aef782df121ca671b74feb706560
dotrungquan <56627093+dotrungquan@users.noreply.github.com>
2022-12-10 17:30:18 +0700

    Update zerossl-cpanel.sh

--- zerossl-cpanel.sh
+++ zerossl-cpanel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Auth: DOTRUNGQUAN.INFO
-read -p "Nhap vao ten mien can cai: " domain
 read -p "Nhao vao UserName cPanel: " user
+read -p "Nhap vao ten mien can cai: " domain
 read -p "Nhap vao Document Root: " document_root
 wget -O - https://get.acme.sh | sh -s email=info@'domain'
 source ~/.bashrc
