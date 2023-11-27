3dddf1c022687b0113a01367a859462a1397b3c6
dotrungquan <dotrungquan.info@gmail.com>
2023-11-27 11:34:35 +0700

    Update zerossl-cpanel.sh

--- zerossl-cpanel.sh
+++ zerossl-cpanel.sh
@@ -4,16 +4,16 @@
 install_ssl_main_domain() {
     read -p "Enter your main domain (e.g., example.com): " main_domain
     wget -O - https://get.acme.sh | sh -s email=ssl@azdigi.info
-    bash /home/*.acme.sh/acme.sh --issue --webroot ~/public_html -d $main_domain -d www.$main_domain --keylength 2048 --force
-    bash /home/*.acme.sh/acme.sh --deploy --deploy-hook cpanel_uapi --domain $main_domain --domain www.$main_domain
+    bash /home/*/.acme.sh/acme.sh --issue --webroot ~/public_html -d $main_domain -d www.$main_domain --keylength 2048 --force
+    bash /home/*/.acme.sh/acme.sh --deploy --deploy-hook cpanel_uapi --domain $main_domain --domain www.$main_domain
 }
 
 # Function to install SSL for addon domain (option 2)
 install_ssl_addon_domain() {
     read -p "Enter your addon domain (e.g., addon.com): " addon_domain
     wget -O - https://get.acme.sh | sh -s email=ssl@azdigi.info
-    bash /home/*.acme.sh/acme.sh --issue --webroot ~/$addon_domain -d $addon_domain -d www.$addon_domain --keylength 2048 --force
-    bash /home/*.acme.sh/acme.sh --deploy --deploy-hook cpanel_uapi --domain $addon_domain --domain www.$addon_domain
+    bash /home/*/.acme.sh/acme.sh --issue --webroot ~/$addon_domain -d $addon_domain -d www.$addon_domain --keylength 2048 --force
+    bash /home/*/.acme.sh/acme.sh --deploy --deploy-hook cpanel_uapi --domain $addon_domain --domain www.$addon_domain
 }
 
 # Main script
